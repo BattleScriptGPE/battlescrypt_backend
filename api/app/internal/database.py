7d43c48f3f976132297c3c9c7fe6234bcc11c744
vignan_q <quentin.vignan@gmail.com>
2023-03-02 17:38:38 +0100

    update for noamd

--- api/app/internal/database.py
+++ api/app/internal/database.py
@@ -2,8 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import logging
+import os
 
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:root@db:3306/gpeDb'
+SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://' + os.environ['DB_USER'] + ':' + os.environ['DB_PASSWORD'] + '@' + os.environ['DB_URL'] + ':3306/gpeDb'
 
 
 engine = create_engine(
