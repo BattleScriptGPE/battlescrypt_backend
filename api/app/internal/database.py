c0a28e72e27ac74a4bc7c214fdcc8eaa46bd3b43
vignan_q <quentin.vignan@gmail.com>
2023-03-08 12:41:17 +0100

    add db_port env var

--- api/app/internal/database.py
+++ api/app/internal/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 import logging
 import os
 
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://' + os.environ['DB_USER'] + ':' + os.environ['DB_PASSWORD'] + '@' + os.environ['DB_URL'] + ':3306/' + os.environ['DB_NAME']
+SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://' + os.environ['DB_USER'] + ':' + os.environ['DB_PASSWORD'] + '@' + os.environ['DB_URL'] + ':' + os.environ['DB_PORT'] + '/' + os.environ['DB_NAME']
 
 
 engine = create_engine(
