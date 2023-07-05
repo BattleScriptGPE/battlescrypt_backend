2e0af3df470e243fb4a03641cfc81c3e2634c4a7
Quentin Vignan <quentinvignan@Quentins-MacBook-Pro.local>
2023-07-05 17:01:13 +0200

    use URL object

--- api/app/utils/database_utils.py
+++ api/app/utils/database_utils.py
@@ -2,6 +2,7 @@ from urllib.parse import quote_plus
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import URL
 import logging
 import os
 
@@ -9,8 +10,16 @@ import os
 ## SQL URL
 SQLALCHEMY_DATABASE_URL = "mariadb+pymysql://" + os.environ["DB_USER"] + ":" + os.environ["DB_PASSWORD"] + "@" + os.environ["DB_URL"] + ":" + os.environ["DB_PORT"] + "/" + os.environ["DB_NAME"] + "?charset=utf8mb4"
 
+url_object = URL.create(
+    "mysql+pymysql",
+    username=os.environ["DB_USER"],
+    password=os.environ["DB_PASSWORD"],  # plain (unescaped) text
+    host=os.environ["DB_URL"],
+    port=int(os.environ["DB_PORT"]),
+    database=os.environ["DB_NAME"],
+)
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(url_object)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
