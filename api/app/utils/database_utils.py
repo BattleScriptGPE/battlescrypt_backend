95eca4f805f9202f6cc6672832d8e639c860a2ed
Quentin Vignan <quentinvignan@quentins-mbp.home>
2023-07-05 15:06:30 +0200

    fix db utils

--- api/app/utils/database_utils.py
+++ api/app/utils/database_utils.py
@@ -3,14 +3,21 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 import logging
+import sqlalchemy
 import os
 
 
 ## SQL URL
-SQLALCHEMY_DATABASE_URL = "mysql://" + os.environ["DB_USER"] + ":" + os.environ["DB_PASSWORD"] + "@" + os.environ["DB_URL"] + ":" + os.environ["DB_PORT"] +  "/" + os.environ["DB_NAME"]
+connect_url = sqlalchemy.engine.url.URL(
+    "mysql+pymysql",
+    username=os.environ["DB_USER"],
+    password=os.environ["DB_PASSWORD"],
+    host=os.environ["DB_URL"],
+    port=os.environ["DB_PORT"],
+    database=os.environ["DB_NAME"],
+)
 
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(connect_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
