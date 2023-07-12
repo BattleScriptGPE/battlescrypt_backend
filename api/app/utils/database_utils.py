b9c5d56b4d89d15ec3f4e5e7608085b2d19ee241
Quentin Vignan <quentinvignan@quentins-mbp.home>
2023-07-12 17:44:39 +0200

    update conf

--- api/app/utils/database_utils.py
+++ api/app/utils/database_utils.py
@@ -9,10 +9,7 @@ import urllib
 
 
 ## SQL URL
-url_object = "mysql+pymysql://{}:{}@{}:{}/{}".format(os.environ["DB_USER"], os.environ["DB_PASSWORD"], os.environ["DB_URL"], os.environ["DB_PORT"], os.environ["DB_NAME"])
-logging.WARNING(os.environ["DB_PASSWORD"])
-logging.WARNING(url_object)
-
+url_object = "mysql+pymysql://{}:{}@{}:{}/{}".format(os.environ["DB_USER"],os.environ["DB_PASSWORD"], os.environ["DB_URL"], os.environ["DB_PORT"], os.environ["DB_NAME"])
 engine = create_engine(url_object)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
--- api/main.py
+++ api/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.router import router
 from fastapi.middleware.cors import CORSMiddleware
 import logging
+import os
 
 app = FastAPI()
 
@@ -18,7 +19,8 @@ origins = [
     "http://localhost",
     "http://localhost:8080",
 ]
-
+url_object = "mysql+pymysql://{}:{}@{}:{}/{}".format(os.environ["DB_USER"],os.environ["DB_PASSWORD"], os.environ["DB_URL"], os.environ["DB_PORT"], os.environ["DB_NAME"])
+logging.INFO(url_object)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
