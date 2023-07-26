ab0c385965a4e32463fbca7f34705c0bc8aa8303
Quentin Vignan <quentinvignan@Quentins-MacBook-Pro.local>
2023-07-26 15:16:30 +0200

    another one

--- api/app/utils/database_utils.py
+++ api/app/utils/database_utils.py
@@ -8,7 +8,7 @@ import os
 
 
 ## SQL URL
-url_object = "mariadb+mariadbconnector://root:XZe&pR5%2397@{}:4406/{}".format(os.environ["DB_USER"],os.environ["DB_PASSWORD"], os.environ["DB_URL"], os.environ["DB_PORT"], os.environ["DB_NAME"])
+url_object = "mariadb+mariadbconnector://root:XZe&pR5%2397@{}:4406/{}".format(os.environ["DB_URL"], os.environ["DB_NAME"])
 engine = create_engine(url_object)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
