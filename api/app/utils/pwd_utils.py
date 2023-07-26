b738160d9c337539903e7f118575b995805fa68f
Quentin Vignan <quentinvignan@Quentins-MacBook-Pro.local>
2023-07-26 16:12:27 +0200

    another one

--- api/app/utils/pwd_utils.py
+++ api/app/utils/pwd_utils.py
@@ -4,12 +4,12 @@ from cryptography.fernet import Fernet
 
 
 def encrypt_password(password: str):
-    fernet = Fernet(os.ENVIRON["SALT"])
+    fernet = Fernet(os.environ["SALT"])
     hash = fernet.encrypt(password.encode())
     return hash
 
 
 def decrypt_password(password: str):
-    fernet = Fernet(os.ENVIRON["SALT"])
+    fernet = Fernet(os.environ["SALT"])
     hash = fernet.decrypt(password).decode()
     return hash
