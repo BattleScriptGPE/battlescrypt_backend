21971131e9a8dcfdd0c564544f04107d35c4acf5
Quentin Vignan <quentinvignan@Quentins-MacBook-Pro.local>
2023-07-26 14:51:08 +0200

    remove logging bug

--- api/main.py
+++ api/main.py
@@ -19,8 +19,7 @@ origins = [
     "http://localhost",
     "http://localhost:8080",
 ]
-url_object = "mysql+pymysql://{}:{}@{}:{}/{}".format(os.environ["DB_USER"],os.environ["DB_PASSWORD"], os.environ["DB_URL"], os.environ["DB_PORT"], os.environ["DB_NAME"])
-logging.INFO(url_object)
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
