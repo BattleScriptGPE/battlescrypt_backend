c0f47128adf85ef25534d6e2e9943a5dc05a4d20
Quentin Vignan <quentinvignan@Quentins-MacBook-Pro.local>
2023-07-26 10:19:00 +0200

    change init

--- databases/init.sql
+++ databases/init.sql
@@ -2,81 +2,75 @@ DROP DATABASE IF EXISTS gpeDb;
 CREATE DATABASE IF NOT EXISTS gpeDb;
 USE gpeDb;
 
-
-CREATE TABLE 'user'
+CREATE TABLE `user`
 (
-    'id'             integer NOT NULL AUTO_INCREMENT ,
-    'firstname'      varchar(45) NOT NULL ,
-    'lastname'       varchar(45) NOT NULL ,
-    'mail'           varchar(150) NOT NULL ,
-    'age'            integer NOT NULL ,
-    'password'       varchar(255) NOT NULL ,
-    'username'       varchar(45) NOT NULL ,
-    'is_admin'       boolean NOT NULL DEFAULT 0,
-    'time_played'    time NOT NULL DEFAULT 0,
-    'wins'           integer NOT NULL DEFAULT 0,
-    'defeats'        integer NOT NULL DEFAULT 0,
-    'line_coded'     integer NOT NULL DEFAULT 0,
-    'last_connexion' time NOT NULL DEFAULT 0,
-    'experience'     integer NOT NULL DEFAULT 0,
-    'created_at'     datetime NOT NULL  DEFAULT NOW(),
-    'updated_at'     datetime NOT NULL DEFAULT NOW(),
-
-    PRIMARY KEY ('id')
+    `id`             INT NOT NULL AUTO_INCREMENT,
+    `firstname`      VARCHAR(45) NOT NULL,
+    `lastname`       VARCHAR(45) NOT NULL,
+    `mail`           VARCHAR(150) NOT NULL,
+    `age`            INT NOT NULL,
+    `password`       VARCHAR(255) NOT NULL,
+    `username`       VARCHAR(45) NOT NULL,
+    `is_admin`       TINYINT(1) NOT NULL DEFAULT 0,
+    `time_played`    INT NOT NULL DEFAULT 0,
+    `wins`           INT NOT NULL DEFAULT 0,
+    `defeats`        INT NOT NULL DEFAULT 0,
+    `line_coded`     INT NOT NULL DEFAULT 0,
+    `last_connexion` TIME NOT NULL DEFAULT '00:00:00',
+    `experience`     INT NOT NULL DEFAULT 0,
+    `created_at`     DATETIME NOT NULL DEFAULT NOW(),
+    `updated_at`     DATETIME NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (`id`)
 );
 
-
-CREATE TABLE 'achievement'
+CREATE TABLE `achievement`
 (
-    'id'          integer NOT NULL AUTO_INCREMENT ,
-    'name'        varchar(45) NOT NULL ,
-    'xp_value'    smallint NOT NULL DEFAULT 0,
-    'description' text NOT NULL ,
-    'metas'       text NOT NULL ,
-
-    PRIMARY KEY ('id')
+    `id`          INT NOT NULL AUTO_INCREMENT,
+    `name`        VARCHAR(45) NOT NULL,
+    `xp_value`    SMALLINT NOT NULL DEFAULT 0,
+    `description` TEXT NOT NULL,
+    `metas`       TEXT NOT NULL,
+    PRIMARY KEY (`id`)
 );
 
-CREATE TABLE 'quest'
+CREATE TABLE `quest`
 (
-    'id'          integer NOT NULL AUTO_INCREMENT ,
-    'name'        varchar(45) NOT NULL ,
-    'description' text NOT NULL ,
-    'number'      smallint NOT NULL ,
-    'difficulty'  tinyint NOT NULL ,
-    'xp_value'    smallint NOT NULL DEFAULT 0,
-
-    PRIMARY KEY ('id')
+    `id`          INT NOT NULL AUTO_INCREMENT,
+    `name`        VARCHAR(45) NOT NULL,
+    `description` TEXT NOT NULL,
+    `number`      SMALLINT NOT NULL,
+    `difficulty`  TINYINT NOT NULL,
+    `xp_value`    SMALLINT NOT NULL DEFAULT 0,
+    PRIMARY KEY (`id`)
 );
 
-CREATE TABLE 'user_achievements'
+CREATE TABLE `user_achievements`
 (
- 'id'             integer NOT NULL AUTO_INCREMENT ,
- 'id_achievement' integer NOT NULL ,
- 'id_user'        integer NOT NULL ,
- 'done_at'        datetime NOT NULL DEFAULT NOW(),
-
-    PRIMARY KEY ('id')
+    `id`             INT NOT NULL AUTO_INCREMENT,
+    `id_achievement` INT NOT NULL,
+    `id_user`        INT NOT NULL,
+    `done_at`        DATETIME NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (`id`)
 );
 
-CREATE TABLE 'user_quests'
+CREATE TABLE `user_quests`
 (
-    'id_quest' integer NOT NULL ,
-    'id_user'  integer NOT NULL ,
-    'status'   enum('done', 'in progress', 'failed') NOT NULL DEFAULT 'in progress',
-    'code'     text NOT NULL ,
-    'done_at'  datetime NOT NULL ,
-
-    KEY 'FK_1' ('id_quest'),
-    CONSTRAINT 'FK_2' FOREIGN KEY 'FK_1' ('id_quest') REFERENCES 'quest' ('id'),
-    KEY 'FK_2' ('id_user'),
-    CONSTRAINT 'FK_3' FOREIGN KEY 'FK_2' ('id_user') REFERENCES 'user' ('id')
+    `id`       INT NOT NULL AUTO_INCREMENT,
+    `id_quest` INT NOT NULL,
+    `id_user`  INT NOT NULL,
+    `status`   ENUM('done', 'in progress', 'failed') NOT NULL DEFAULT 'in progress',
+    `code`     TEXT NOT NULL,
+    `done_at`  DATETIME NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `FK_quest` (`id_quest`),
+    KEY `FK_user` (`id_user`),
+    CONSTRAINT `FK_quest` FOREIGN KEY (`id_quest`) REFERENCES `quest` (`id`),
+    CONSTRAINT `FK_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`)
 );
 
-CREATE TABLE 'download'
+CREATE TABLE `download`
 (
-    'id' integer NOT NULL AUTO_INCREMENT ,
-    'nb_download' integer NOT NULL ,
-
-    PRIMARY KEY ('id')
+    `id`          INT NOT NULL AUTO_INCREMENT,
+    `nb_download` INT NOT NULL,
+    PRIMARY KEY (`id`)
 );
